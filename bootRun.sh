117a4d0513fbbbff24640b5a65975c7835e650ac
Naga <sailurao@yahoo.com>
2020-05-01 21:00:07 -0400

    Initial commit

---
+++ bootRun.sh
@@ -0,0 +1,18 @@
+#clean current process running on 8080
+echo "killing current process running on 8080..."
+fuser -n tcp -k 8080
+
+#test comment to test the auto builds
+echo "Test# 30"
+
+#sleep for 10 seconds to kill the process
+sleep 10
+
+#clean temporary gradle directory
+echo "Removing temporary gradle directory..."
+rm -rf .gradle
+
+#run spring boot in background
+echo "Running gradle script to build the project..."
+cd /var/lib/jenkins/workspace/GradleSpringBoot
+nohup gradle bootRun >> /tmp/bootRun.log 2>&1 &
